c6282abb152f4a04c0448926c8e8fe405364950d
stephaaniie <stephanie.castillo@gmail.com>
2020-07-29 11:51:26 -0300

    Se probo las funcionalidades con postman falta añadir test unitario

--- src/main/java/ar/com/api/ada/nasa/nasa/controllers/PaisController.java
+++ src/main/java/ar/com/api/ada/nasa/nasa/controllers/PaisController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,7 @@ import ar.com.api.ada.nasa.nasa.services.implementations.TemperaturaService;
 
 
 @RestController
+@RequestMapping("/api")
 public class PaisController {
     @Autowired
     PaisService sPais;
